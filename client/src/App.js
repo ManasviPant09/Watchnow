f485ff1bf21ce1b5ba9e12298d84d88dfae53c45
Manasvi Pant <manasvipant1106@gmail.com>
2023-02-25 17:06:42 +0530

    Updated Signup,Login functionality with Firebase

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import {BrowserRouter,Route,Routes} from "react-router-dom";
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Home from './pages/Home';
-import GetStarted from './pages/GetStarted';
 const App = () => {
   return (
     <BrowserRouter>
